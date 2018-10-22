6549d13217a261b29537810b26e74a8cab3d29c9
ELIZABETH LE <elizabethlemk@yahoo.com>
2018-10-22 23:14:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,3 +24,10 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 
   var theFunk = funkyFunction();
+function funkyFunction(){
+  var funkyFunction = function() {
+    return function() {
+      return "FUNKY!"
+    }
+  }
+}
